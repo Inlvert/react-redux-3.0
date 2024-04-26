78b204a16b63293153a5dbd003d2d9825d1f1201
Borys Ushakov <inlvert@gmail.com>
2024-04-26 11:16:52 +0300

    add change for TakeAllCars

--- src/components/TakeAllCars/index.jsx
+++ src/components/TakeAllCars/index.jsx
@@ -7,21 +7,24 @@ const initialValues = {
   cars: [],
 };
 
-const TakeAllCars = ({ findAllCarRequest }) => {
+const TakeAllCars = (props) => {
+  const { cars, findAllCarRequest } = props;
   const hendleSubmit = (values, formikBag) => {
     const cars = {
       ...values,
     };
     findAllCarRequest(cars);
-    console.log(values)
-
+    console.log(props);
   };
 
-  // const carList = cars.map((car) => (
-  //   <li>
-  //     <h1 ></h1>
-  //   </li>
-  // ))
+  const carList = cars.map((car, index) => (
+    <li key={index}>
+      <div>
+        <h1>{`${car.manufacturer}, ${car.model}, ${car.km} - km mileage, ${car.color} - color,`}</h1>
+        <h3>{`${car.year} prodaction`}</h3>
+      </div>
+    </li>
+  ));
 
   return (
     <div>
@@ -31,6 +34,7 @@ const TakeAllCars = ({ findAllCarRequest }) => {
           <button type="submit">all cars from DB</button>
         </Form>
       </Formik>
+      <ul>{carList}</ul>
     </div>
   );
 };
